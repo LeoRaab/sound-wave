a426426c8473da9f95f2844c5705fe607afee4be
LeoRaab <leopold.raab@gmail.com>
2022-08-14 11:42:58 +0200

    added react-spring for focus animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import { Canvas } from '@react-three/fiber';
 import Wave from './common/components/Wave';
 import Sound from './common/components/Sound';
+import { useState } from 'react';
+import { useSpring } from '@react-spring/three';
 
 const App = () => {
+  const [ticks, setTicks] = useState(0);
+  const { ticksSpring, clickSpring } = useSpring({
+    ticksSpring: ticks, // Springy tick value (each click / release is a tick)
+    clickSpring: ticks % 2 === 1 ? 1 : 0, // Springy click factor (1 means clicked, 0 means released)
+    config: { tension: 20, friction: 20, clamp: true },
+  });
+  const bind = {
+    onPointerDown: (e: any) => {
+      // Capture the pointer so it's still tracked outside the window
+      e.target.setPointerCapture(e.pointerId);
+      setTicks(ticks + 1);
+    },
+    onPointerUp: () => {
+      // Prevent misfires
+      if (ticks % 2 === 1) {
+        // Only finish the animation if held down for long enough
+        if (clickSpring.get() > 0.5) setTicks(ticks + 1);
+        // Otherwise undo the contraction (this way you can't speed up the animation by spam clicking)
+        else setTicks(ticks - 1);
+      }
+    },
+  };
 
   return (
-    <Canvas orthographic camera={{ zoom: 20 }}>
+    <Canvas orthographic camera={{ zoom: 20 }} resize={{ polyfill: ResizeObserver }} {...bind}>
       <color attach="background" args={['black']} />
-      <Wave />
+      <Wave ticksSpring={ticksSpring} clickSpring={clickSpring} />
       <Sound />
     </Canvas>
   );
--- src/common/components/Wave.tsx
+++ src/common/components/Wave.tsx
@@ -1,5 +1,6 @@
-import { useFrame, useThree } from '@react-three/fiber';
-import { useRef, useMemo, useLayoutEffect, useState } from 'react';
+import { useRef, useMemo } from 'react';
+import { SpringValue } from '@react-spring/three';
+import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import { InstancedMesh } from 'three';
 
@@ -7,16 +8,32 @@ const roundedSquareWave = (t: number, delta: number, a: number, f: number) => {
   return ((2 * a) / Math.PI) * Math.atan(Math.sin(2 * Math.PI * t * f) / delta);
 };
 
-const Wave = () => {
-  const { scene, raycaster, pointer, camera, mouse } = useThree();
-  
+type WaveProps = {
+  ticksSpring: SpringValue<number>;
+  clickSpring: SpringValue<number>;
+};
+
+const Wave = ({ ticksSpring, clickSpring }: WaveProps) => {
   const meshRef = useRef<InstancedMesh>(null);
 
-  const { vec, transform, positions, distances } = useMemo(() => {
+  const { vec, right, transform, vec3Mouse, focus, positions } = useMemo(() => {
+    // Variables for intermediary calculations
     const vec = new THREE.Vector3();
     const transform = new THREE.Matrix4();
+
+    // Vector pointing to the right (for angle calculations)
+    const right = new THREE.Vector3(1, 0, 0);
+
+    // True cursor position in 3D space
+    const vec3Mouse = new THREE.Vector3();
+
+    // Where the dots are clustered around
+    const focus = new THREE.Vector3();
+
+    // Precompute randomized initial positions (array of Vector3)
     const positions = [...Array(10000)].map((_, i) => {
       const position = new THREE.Vector3();
+
       // Place in a grid
       position.x = (i % 100) - 50;
       position.y = Math.floor(i / 100) - 50;
@@ -29,41 +46,46 @@ const Wave = () => {
       position.y += Math.random() * 0.3;
       return position;
     });
-    const distances = positions.map((pos) => pos.length());
-    return { vec, transform, positions, distances };
+    return { vec, right, transform, vec3Mouse, focus, positions };
   }, []);
-
-  // useFrame(({ clock }) => {
-  //   for (let i = 0; i < 10000; ++i) {
-  //     const t = clock.elapsedTime - distances[i] / 80;
-  //     const wave = roundedSquareWave(t, 0.1, 1, 1 / 4);
-  //     const scale = 1 + wave * 0.3;
-  //     vec.copy(positions[i]).multiplyScalar(scale);
-  //     transform.setPosition(vec);
-  //     meshRef.current!.setMatrixAt(i, transform);
-  //   }
-  //   meshRef.current!.instanceMatrix!.needsUpdate = true;
-  // });
-
-  useFrame(({ raycaster, pointer, camera, scene }) => {
-    for (let i = 0; i < 10000; ++i) {
-      vec.copy(positions[i]);
-      transform.setPosition(vec);
-      meshRef.current!.setMatrixAt(i, transform);
+  useFrame(({ mouse, viewport }) => {
+    if (!meshRef.current) {
+      return;
     }
+
+    // Convert mouse screen coords to 3D scene coords
+    vec3Mouse.x = (mouse.x * viewport.width) / 2;
+    vec3Mouse.y = (mouse.y * viewport.height) / 2;
+
+    for (let i = 0; i < 10000; ++i) {
+      // Drift focus to center as click is released
+      focus.copy(vec3Mouse).multiplyScalar(clickSpring.get());
+
+      // Vec holds the dot position relative to the focus point
+      vec.copy(positions[i]).sub(focus);
+
+      // Same distance calculation as original demo
+      const dist = vec.length() + Math.cos(vec.angleTo(right) * 8) * 0.5;
+
+      // This adjusts the wave input to set a suitable phase and frequency
+      const t = ticksSpring.get() / 2 + 1 / 2 - dist / 100;
+      const wave = roundedSquareWave(t, 0.15 + (0.2 * dist) / 72, 0.4, 1);
+
+      // Scale dot position relative to the focus point
+      vec.multiplyScalar(wave + 1.3).add(focus);
+
+      // Set the instance's transformation matrix
+      transform.setPosition(vec);
+      meshRef.current.setMatrixAt(i, transform);
+    }
+    meshRef.current.instanceMatrix.needsUpdate = true;
   });
 
-  const onPointerMove = () => {
-   
-  };
-
   return (
-    <group onPointerMove={onPointerMove}>
-      <instancedMesh ref={meshRef} args={[undefined, undefined, 10000]}>
-        <circleBufferGeometry args={[0.15]} />
-        <meshBasicMaterial />
-      </instancedMesh>
-    </group>
+    <instancedMesh ref={meshRef} args={[undefined, undefined, 10000]}>
+      <circleBufferGeometry args={[0.15]} />
+      <meshBasicMaterial />
+    </instancedMesh>
   );
 };
 
