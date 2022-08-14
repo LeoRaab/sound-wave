c67ae3e9d317416bd32226e40957ed2e48461b9a
LeoRaab <leopold.raab@gmail.com>
2022-08-14 11:28:50 +0200

    added filter

--- src/common/components/Sound.tsx
+++ src/common/components/Sound.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useFrame, useLoader, useThree } from '@react-three/fiber';
 import * as THREE from 'three';
 import useTouch from '../hooks/use-touch';
@@ -7,27 +7,36 @@ import soundFile from '../../assets/sounds/sound.mp3';
 const Sound = () => {
   const { camera } = useThree();
   const { isTouched } = useTouch();
+
   const buffer = useLoader(THREE.AudioLoader, soundFile);
   const listener = useRef<THREE.AudioListener>(new THREE.AudioListener());
+  const audio = useRef<THREE.Audio>(new THREE.Audio(listener.current));
+
   camera.add(listener.current);
-  const sound = useRef<THREE.Audio>(new THREE.Audio(listener.current));
+
+  const lowPassFilter = new BiquadFilterNode(audio.current.context, { type: 'lowpass', frequency: 10000 });
+  audio.current.setFilter(lowPassFilter);
 
   useFrame(({ pointer }) => {
-    if (buffer && isTouched && !sound.current.isPlaying) {
-      sound.current.setBuffer(buffer);
-      sound.current.setLoop(true);
-      sound.current.play();
+    if (buffer && isTouched && !audio.current.isPlaying) {
+      audio.current.setBuffer(buffer);
+      audio.current.setLoop(true);
+      audio.current.play();
     }
 
-    if (sound.current.isPlaying && isTouched) {
-      console.log(pointer);
+    if (audio.current.isPlaying && isTouched) {
+      if (pointer.x > 0.1) {
+        lowPassFilter.frequency.value = 132;
+      } else {
+        lowPassFilter.frequency.value = 10000;
+      }
     }
 
-    if (sound.current.isPlaying && !isTouched) {
-      sound.current.pause();
+    if (audio.current.isPlaying && !isTouched) {
+      audio.current.pause();
     }
   });
-  
+
   return <></>;
 };
 
